9090196a1d65a224aa76ad1909180d3053d973c3
yang0299 github <yang0299@algonquinlive.com>
2021-02-16 19:47:37 -0500

    lab3 done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Lab3.Models;
+using Microsoft.AspNetCore.Http;
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Razor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Count()
+        {
+            
+        //    var number = Convert.ToInt32(Request.Form["num"]);
+           // ViewData["num"] = number;
+            ViewData["num"] = Convert.ToInt32(Request.Form["num"]);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PlayAgain()
+        {
+            return View("Razor");
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            return View(person);
+        }
+        [HttpGet]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        
+        public string FirstName
+        {
+            get;
+            set; 
+        }
+       
+        public string LastName
+        {
+            get;
+            set;
+        }
+       
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> DOB
+        {
+            get;
+            set;
+        } 
+
+        [Required]
+        [StringLength(20)]
+        [BindProperty(Name = "pass")]
+        public string Password
+        {
+            get;
+            set;
+        }
+        [Required]
+        [BindProperty(Name = "descrip")]
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
